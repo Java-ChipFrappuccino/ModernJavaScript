ffbdfeff4c387f4ffc4dcff675fe05833887655c
Joon <pss21242124@gmail.com>
2025-01-10 01:08:05 +0900

    25.01.10

--- alert.js
+++ alert.js
@@ -1,1 +1,15 @@
 alert('23');
+
+// 주석을 생활화
+// 변수명은 알기쉽지만 명확하게
+let admin;
+let name;
+
+name = 'john';
+admin = name;
+alert(admin);
+
+let ourPlanetName = 'earth';
+let currentUserName;
+// 상수는 이미 값을 알고있고 절대 변하지 않을값이라면 주민번호, 생일같이, 나이같은건 변화가 있으니 ㄴㄴ
+const BIRTHDAY = 17520922;
